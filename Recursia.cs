926865667187996326e91d4756d4984bd280f24f
Polina <polinanadahovskaya@gmail.com>
2025-02-17 19:37:15 +0300

    test

--- Recursia.cs
+++ Recursia.cs
@@ -1,30 +1,31 @@
-﻿public interface IExample
+﻿using System;
+
+class Program
 {
-    void Method();
+    static void Main(string[] args) {
+      const int buffer_size = 10;
+      char[] buf = new char[buffer_size];
 
-    void Log(string message) {
-        Console.WriteLine("test1");
-    }
-    int Property {get; set;}
-}
+      if(args.Length == 0) {
+        Console.WriteLine("Args no");
+         return
+      }
 
-public interface Itest : IExample
-{
-    void Testik();
-}
-
-public class ExampleClass : Itest
-{
-      public void Method() => Console.WriteLine("test");
-    public void Testik() => Console.WriteLine("Testik");
-    
-    public int Property { get; set;}
-}
-
-class Program {
-    static void Main() {
-        ExampleClass obj = new ExampleClass();
-        obj.Method();
-        obj.Testik();
+      foreach(var arg in args)
+      {
+        try{
+            if(arg.Length > buffer_size){
+                throw ex OverflowException("Very long String");
+            }
+            for(int i = 0; i < arg.Length; i++){
+                buf[i] = arg[i];
+            }
+            Console.WriteLine($"buf super {new string(buf, 0, arg.Length)}");
+        }
+        catch(OverflowException ex) {
+            Console.WriteLine(ex.Message)
+        }
+      }
+     
     }
 }
